c884504f5460854f7ec9662937dee90936ab397e
Petr Ignatov <petrignatovefr@gmail.com>
2025-11-18 13:06:44 +0000

    Add basic minigame API, prepare for adding display

--- src/wonderbox.h
+++ src/wonderbox.h
@@ -1,5 +1,18 @@
 #ifndef WONDERBOX_H
-#define WONDERBOX_H
+#define WONDERBOX_H 
+
+typedef struct {
+	unsigned char **idle;
+	unsigned char **move;
+	unsigned int idle_frames;
+	unsigned int move_frames;
+	unsigned int cur_frames;
+	unsigned char xsz;
+	unsigned char ysz;
+	unsigned char x;
+	unsigned char y;
+	void (*update)(void);
+} object_t;
 
 void timer_init(int div);
 void led_init(void);
@@ -9,5 +22,7 @@ void led_print(int col);
 void buzz(void);
 void adc_init(void);
 unsigned int adc_read(int pin);
+void init(void);
+void update(void);
 
 #endif
